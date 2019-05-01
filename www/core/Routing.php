48333b45264643c55d4cf31d8985dc7a5a72185f
RayapinA <rayapin.antoine36@gmail.com>
2019-05-01 17:01:18 +0200

    Naming

--- www/core/Routing.php
+++ www/core/Routing.php
@@ -9,6 +9,8 @@ class Routing
 
     public static function getRoute(string $slug):array
     {
+        //Don't use else Keyword && One level indentation
+
         $routes = yaml_parse_file(self::$routeFile);
 
         if (!isset($routes[$slug])) {
@@ -19,23 +21,23 @@ class Routing
             die('Il y a une erreur dans le fichier routes.yml');
         }
 
-        $c = ucfirst($routes[$slug]['controller']).'Controller';
-        $a = $routes[$slug]['action'].'Action';
-        $cPath = 'controllers/'.$c.'.php';
+        $controller = ucfirst($routes[$slug]['controller']).'Controller';
+        $action = $routes[$slug]['action'].'Action';
+        $controllerPath = 'controllers/'.$controller.'.php';
 
-        return ['c' => $c, 'a' => $a, 'cPath' => $cPath];
+        return ['c' => $controller, 'a' => $action, 'cPath' => $controllerPath];
 
     }
 
-    public static function getSlug(string $c, string $a):string
+    public static function getSlug(string $controller, string $action):string
     {
         $routes = yaml_parse_file(self::$routeFile);
 
-        foreach ($routes as $slug => $cAndA) {
-            if (!empty($cAndA['controller']) &&
-                !empty($cAndA['action']) &&
-                $cAndA['controller'] == $c &&
-                $cAndA['action'] == $a) {
+        foreach ($routes as $slug => $data) {
+            if (!empty($data['controller']) &&
+                !empty($data['action']) &&
+                $data['controller'] == $controller &&
+                $data['action'] == $action) {
                 return $slug;
             }
         }
